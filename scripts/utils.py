8c081b697247da1fee6c60bae283b81981bb4245
Stephen Garlick <sgarlick@taximagic.com>
2015-02-17 10:22:00 -0500

    renames

---
+++ scripts/utils.py
@@ -0,0 +1,44 @@
+import logging
+import loggly.handlers
+import os
+
+LOGGLY_URL = "https://logs-01.loggly.com/inputs/" + \
+             "{}/tag/python,boot,cloudformation,{}"
+
+
+def get_logger(name, tags):
+    return build_logger(name, os.environ['LOGGLY_TOKEN'], tags)
+
+
+def build_logger(name, loggly_token, tags):
+    """ Sets up a logger to send files to Loggly with dynamic tags """
+    logger = logging.getLogger(name)
+    url = LOGGLY_URL.format(loggly_token, ",".join(tags))
+    handler = loggly.handlers.HTTPSHandler(url)
+    logger.addHandler(handler)
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+class InstanceTags(object):
+    def __init__(self, ec2_conn, instance_id):
+        self.tags = ec2_conn.get_only_instances(instance_id)[0].tags
+
+    def get(self, tag_name):
+        return self.tags[tag_name]
+
+    def get_role(self):
+        return self.get('Role').lower()
+
+    def get_name(self):
+        return self.get('Name')
+
+    def get_environment(self):
+        return self.get('Environment')
+
+    def get_public_internal_hosted_zone(self):
+        return self.get('PublicInternalHostedZone')
+
+    def get_public_internal_domain(self):
+        return self.get('PublicInternalDomain')
---
+++ scripts/utils_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import MagicMock
+import utils
+
+
+class UtilsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.logger_name = UtilsTest.__name__
+        self.instance_id = 'i-abc123'
+        self.loggly_token = 'abc123'
+        self.role = 'role'
+        self.tags = [self.instance_id, self.role]
+        self.conn = MagicMock()
+        self.instance = MagicMock()
+        self.instance.tags = {'Role': self.role, 'Name': self.logger_name,
+                              'PublicInternalHostedZone': 'PublicInternalHostedZone',
+                              'PublicInternalDomain': 'PublicInternalDomain'}
+        self.instances = [self.instance]
+        self.conn.get_only_instances = MagicMock(return_value=self.instances)
+        self.instance_tags = utils.InstanceTags(self.conn, self.instance_id)
+
+    def test_build_logger(self):
+        logger = utils.build_logger(
+            self.logger_name, self.loggly_token, self.tags)
+        self.assertEqual(self.logger_name, logger.name)
+        self.assertIn(self.loggly_token, logger.handlers[0].url)
+        for tag in self.tags:
+            self.assertIn(tag, logger.handlers[0].url)
+
+    def test_get_role(self):
+        role = self.instance_tags.get_role()
+        self.conn.get_only_instances.assert_called_with(self.instance_id)
+        self.assertEqual(role, self.role)
+
+    def test_get_name(self):
+        name = self.instance_tags.get_name()
+        self.conn.get_only_instances.assert_called_with(self.instance_id)
+        self.assertEqual(name, self.logger_name)
+
+    def test_public_internal_hosted_zone(self):
+        name = self.instance_tags.get_public_internal_hosted_zone()
+        self.conn.get_only_instances.assert_called_with(self.instance_id)
+        self.assertEqual(name, 'PublicInternalHostedZone')
+
+    def test_public_internal_domain(self):
+        name = self.instance_tags.get_public_internal_domain()
+        self.conn.get_only_instances.assert_called_with(self.instance_id)
+        self.assertEqual(name, 'PublicInternalDomain')
+
+if __name__ == '__main__':
+    unittest.main()
